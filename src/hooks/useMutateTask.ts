30104e05f99947ba820c88a867f88c7b954e147c
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-27 23:00:33 +0900

    Update用の関数を作成。

--- src/hooks/useMutateTask.ts
+++ src/hooks/useMutateTask.ts
@@ -36,4 +36,41 @@ export const useMutateTask = () => {
       },
     }
   );
+
+  const updateTaskMutation = useMutation(
+    (task: Task) =>
+      axios.put<Task>(
+        `${process.env.REACT_APP_API_URL}/todo/${task.id}`,
+        {
+          title: task.title,
+          description: task.description,
+        },
+        { withCredentials: true }
+      ),
+    {
+      onSuccess: (res, variables) => {
+        const previousTodos = queryClient.getQueryData<Task[]>('tasks');
+        if (previousTodos) {
+          queryClient.setQueryData<Task[]>(
+            'tasks',
+            previousTodos.map((task) =>
+              task.id === variables.id ? res.data : task
+            )
+          );
+        }
+        dispatch(resetEditedTask());
+      },
+      onError: (err: any) => {
+        alert(`${err.response.data.detail}\n${err.message}`);
+        if (
+          err.response.data.detail === 'The JWT has expired' ||
+          err.response.data.detail === 'The CSRF token has expired.'
+        ) {
+          dispatch(toggleCsrfState());
+          dispatch(resetEditedTask());
+          history.push('/');
+        }
+      },
+    }
+  );
 };
