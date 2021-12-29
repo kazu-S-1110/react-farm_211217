2188b2b1a3d3f6bceb18eda778fd1c9d09ac64e7
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-29 22:00:40 +0900

    TaskItemコンポーネントを作成。

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,39 @@
+import { VFC, memo } from 'react';
+import { PencilAltIcon, TrashIcon } from '@heroicons/react/solid';
+import { Task } from '../types/types';
+import { useAppDispatch } from '../app/hooks';
+import { useMutateTask } from '../hooks/useMutateTask';
+import { setEditedTask } from '../slices/appSlice';
+
+const TaskItemMemo: VFC<Task> = ({ id, title, description }) => {
+  const dispatch = useAppDispatch();
+  const { deleteTaskMutation } = useMutateTask();
+
+  return (
+    <li>
+      <span className="font-bold cursor-pointer">{title}</span>
+      <div className="flex float-right ml-20">
+        <PencilAltIcon
+          className="h-5 w-5 mx-1 text-blue-500 cursor-pointer"
+          onClick={() => {
+            dispatch(
+              setEditedTask({
+                id: id,
+                title: title,
+                description: description,
+              })
+            );
+          }}
+        />
+        <TrashIcon
+          className="h-5 w-5 to-blue-500 cursor-pointer"
+          onClick={() => {
+            deleteTaskMutation.mutate(id);
+          }}
+        />
+      </div>
+    </li>
+  );
+};
+
+export const TaskItem = memo(TaskItemMemo);
