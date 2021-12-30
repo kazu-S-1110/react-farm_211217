15e2ea23b3cfe45f3db290fff8062d2e076fa20d
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-30 18:18:58 +0900

    入力フォームを作成。

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,19 +1,61 @@
 import { LogoutIcon } from '@heroicons/react/outline';
 import { VFC } from 'react';
+import { ShieldCheckIcon } from '@heroicons/react/solid';
 import { useProcessAuth } from '../hooks/useProcessAuth';
 import { useQueryTasks } from '../hooks/useQueryTasks';
 import { useQueryUser } from '../hooks/useQueryUser';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { selectTask, setEditedTask } from '../slices/appSlice';
+import { useProcessTask } from '../hooks/useProcessTask';
 
 export const Todo: VFC = () => {
   const { logout } = useProcessAuth();
   const { data: dataUser } = useQueryUser();
   const { data: dataTasks, isLoading: isLoadingTasks } = useQueryTasks();
+  const dispatch = useAppDispatch();
+  const editedTask = useAppSelector(selectTask);
+  const { processTask } = useProcessTask();
+
   return (
     <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
+      <div className="flex items-center">
+        <ShieldCheckIcon className="h-8 w-8 mr-3 text-green-500 cursor-pointer" />
+        <span className="text-center text-3xl font-extrabold">CRUD tasks</span>
+      </div>
+      <p className="my-3 text-sm">{dataUser?.email}</p>
+
       <LogoutIcon
         onClick={logout}
         className="h-7 w-7 mb-5 to-blue-500 cursor-pointer"
       />
+      <form onSubmit={processTask}>
+        <input
+          type="text"
+          placeholder="title ?"
+          className="mb-3 mr-3 px-3 py-2 border border-gray-300"
+          onChange={(e) =>
+            dispatch(setEditedTask({ ...editedTask, title: e.target.value }))
+          }
+          value={editedTask.title}
+        />
+        <input
+          type="text"
+          placeholder="description ?"
+          className="mb-3  px-3 py-2 border border-gray-300"
+          onChange={(e) =>
+            dispatch(
+              setEditedTask({ ...editedTask, description: e.target.value })
+            )
+          }
+          value={editedTask.description}
+        />
+        <button
+          disabled={!editedTask.title || !editedTask.description}
+          className="disabled:opacity-40 mx-3 py-2 px-3 text-white bg-indigo-600 rounded"
+        >
+          {editedTask.id === '' ? 'Create' : 'Update'}
+        </button>
+      </form>
     </div>
   );
 };
